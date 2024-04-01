132a3a4d516e67287c2a0089309f588b18b0b24b
Tommy Woodley <66870296+TommyWoodley@users.noreply.github.com>
2024-04-01 17:48:01 +0100

    FYP-62: Split into 2 level system - position wrapper (#27)
    * add position wrapper
    
    * pep8 fixes

---
+++ 04_Repository/Gym/Wrappers/position_wrapper.py
@@ -0,0 +1,45 @@
+import gymnasium as gym
+from gymnasium import spaces
+from typing import Dict, Any, Tuple
+import numpy as np
+
+
+class PositionWrapper(gym.Wrapper):
+    MAGNITUDE = 0.001
+
+    def __init__(self, env) -> None:
+        super().__init__(env)
+
+        # Position Based Action Space
+        self.action_space = spaces.Box(low=np.array([-5, 0]), high=np.array([5, 5]), dtype=np.float32)
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(2,), dtype=np.float32)
+        self.current_state = None
+
+    def step(self, action: np.ndarray) -> Tuple[np.ndarray, float, bool, bool, Dict[Any, Any]]:
+        print("Action: ", action)
+        state, reward, terminated, truncated, info = self.take_single_step(action)
+
+        while not PositionWrapper.is_close_enough(self.current_state, action):
+            state, reward, terminated, truncated, info = self.take_single_step(action)
+
+        return state, reward, terminated, truncated, info
+
+    def reset(self, seed: int = None, options: Dict[Any, Any] = None) -> Tuple[np.ndarray, Dict[Any, Any]]:
+        state, info = self.env.reset(seed, options)
+        self.current_state = state
+        return state, info
+
+    def is_close_enough(curr_pos, target_pos, threshold=0.01) -> bool:
+        distance = np.linalg.norm(curr_pos - target_pos)
+        return distance <= threshold
+
+    def take_single_step(self, target: np.ndarray) -> Tuple[np.ndarray, float, bool, bool, Dict[Any, Any]]:
+        # Calculate direction vector
+        direction = target - self.current_state
+        action = np.clip(direction, -self.MAGNITUDE, self.MAGNITUDE)
+
+        state, reward, terminated, truncated, info = self.env.step(action)
+
+        self.current_state = state
+
+        return state, reward, terminated, truncated, info
--- 04_Repository/main.py
+++ 04_Repository/main.py
@@ -1,90 +1,21 @@
 # %%
-from Gym.simple_drone_env import SimpleDroneEnv
 from Gym.bullet_drone_env import BulletDroneEnv
 from Gym.Wrappers.two_dim_wrapper import TwoDimWrapper
+from Gym.Wrappers.position_wrapper import PositionWrapper
 from stable_baselines3 import SAC
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.noise import NormalActionNoise
 import numpy as np
 
 # %%
-# Check simple model conforms to gym env
-
-env = SimpleDroneEnv()
-check_env(env, warn=True)
-
-# %%
-
-env = SimpleDroneEnv()
-
-obs, _ = env.reset()
-env.render()
-
-print(env.observation_space)
-print(env.action_space)
-print(env.action_space.sample())
-
-n_steps = 20
-for step in range(n_steps):
-    print(f"Step {step + 1}")
-    obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
-    done = terminated or truncated
-    print("obs=", obs, "reward=", reward, "done=", done)
-    env.render()
-    if done:
-        print("Goal reached!", "reward=", reward)
-        break
-# %%
-
-env = SimpleDroneEnv()
-n_actions = env.action_space.shape[-1]
-action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.4 * np.ones(n_actions))
-
-default_model = SAC(
-    "MlpPolicy",
-    env,
-    verbose=1,
-    seed=0,
-    batch_size=64,
-    action_noise=action_noise,
-    policy_kwargs=dict(net_arch=[64, 64]),
-).learn(50_000)
-
-# %%
-# Test the trained agent
-# using the vecenv
-obs, _ = env.reset()
-n_steps = 20
-for step in range(n_steps):
-    action, _ = default_model.predict(obs, deterministic=True)
-    print(f"Step {step + 1}")
-    print("Action: ", action)
-    state, reward, terminated, truncated, info = env.step(action)
-    done = terminated or truncated
-    print("obs=", obs, "reward=", reward, "done=", done)
-    env.render()
-    if done:
-        # Note that the VecEnv resets automatically
-        # when a done signal is encountered
-        print("Goal reached!", "reward=", reward)
-        break
-
-# %%
-# Check BulletDroneEnv() conforms to gym.Env
-
-env = BulletDroneEnv()
-check_env(env, warn=True)
-env.close()
-
-# %%
 # Check BulletDroneEnv in 2d wrapper confors to gym.Env
-env = TwoDimWrapper(BulletDroneEnv())
+env = PositionWrapper(TwoDimWrapper(BulletDroneEnv()))
 check_env(env, warn=True)
 env.close()
 
 # %%
 
-env = BulletDroneEnv()
+env = PositionWrapper(TwoDimWrapper(BulletDroneEnv()))
 n_actions = env.action_space.shape[-1]
 action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.0001 * np.ones(n_actions))
 
